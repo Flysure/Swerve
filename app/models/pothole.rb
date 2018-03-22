8cf2b783817d0c85c1f4f6e5d17e14305b09edb2
Andrew <flysurea@gmail.com>
2018-03-22 13:30:45 -0400

    hide tags on popup

--- app/models/pothole.rb
+++ app/models/pothole.rb
@@ -1,5 +1,5 @@
 class Pothole < ApplicationRecord
   belongs_to :user
-  reverse_geocoded_by :latitude, :longitude, :address => :loc  # ActiveRecord
+  reverse_geocoded_by :latitude, :longitude, :address => :location  # ActiveRecord
   after_validation :reverse_geocode  # auto-fetch address
 end
