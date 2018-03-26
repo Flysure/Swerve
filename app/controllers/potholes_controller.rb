49514c9295f9226f45fc430593e2707a08a182f1
Andrew <flysurea@gmail.com>
2018-03-26 13:39:45 -0400

    refactor infowindow partial

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -13,7 +13,7 @@ class PotholesController < ApplicationController
     @hash = Gmaps4rails.build_markers(@potholes) do |pothole, marker|
       marker.lat pothole.latitude
       marker.lng pothole.longitude
-      marker.infowindow render_to_string(:partial => "/potholes/my_template", :locals => { :@pothole => pothole})
+      marker.infowindow render_to_string(:partial => "/potholes/infowindow", :locals => { :pothole => pothole})
     end
   end
   def show
