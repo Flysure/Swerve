301a6dc6a3a209297b4f7af07395e9e8eda5f764
Andrew <flysurea@gmail.com>
2018-05-04 13:20:00 -0400

    move function into helper

--- app/controllers/potholes_controller.rb
+++ app/controllers/potholes_controller.rb
@@ -1,7 +1,6 @@
 class PotholesController < ApplicationController
 
   def create
-
     @pothole = Pothole.new(latitude:params[:pothole][:latitude],
       longitude:params[:pothole][:longitude],
       user_id:params[:pothole][:user_id],
@@ -13,11 +12,7 @@ class PotholesController < ApplicationController
     @distance = 25;
     @pothole = Pothole.new
     @potholes = Pothole.all
-    @hash = Gmaps4rails.build_markers(@potholes) do |pothole, marker|
-      marker.lat pothole.latitude
-      marker.lng pothole.longitude
-      marker.infowindow render_to_string(:partial => "/potholes/partials/infowindow", :locals => { :pothole => pothole})
-    end
+    dropMarkers
   end
   def show
     @pothole = Pothole.find(params[:id])
--- app/helpers/potholes_helper.rb
+++ app/helpers/potholes_helper.rb
@@ -1,2 +1,9 @@
 module PotholesHelper
+  def dropMarkers
+    @hash = Gmaps4rails.build_markers(@potholes) do |pothole, marker|
+      marker.lat pothole.latitude
+      marker.lng pothole.longitude
+      marker.infowindow render_to_string(:partial => "/potholes/partials/infowindow", :locals => { :pothole => pothole})
+    end
+  end
 end
